b6686b6e08ebde7e94ace039c3df58440ca186f3
LukasKrisna <krisnaprasriyan@gmail.com>
2020-11-06 20:00:15 +0700

    RecyclerView dengan CardView

--- app/src/main/java/com/lukas/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/lukas/myrecyclerview/MainActivity.kt
@@ -10,6 +10,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val list = ArrayList<Hero>()
+    private var title = "Mode List"
+    private var mode: Int = 0
+
+    companion object {
+        private const val STATE_TITLE = "state_string"
+        private const val STATE_LIST = "state_list"
+        private const val STATE_MODE = "state_mode"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,22 @@ class MainActivity : AppCompatActivity() {
         rv_heroes.setHasFixedSize(true)
         list.addAll(getListHeroes())
         showRecyclerList()
+        setActionBarTitle(title)
+        if (savedInstanceState == null) {
+            setActionBarTitle(title)
+            list.addAll(getListHeroes())
+            showRecyclerList()
+            mode = R.id.action_list
+        } else {
+            title = savedInstanceState.getString(STATE_TITLE).toString()
+            val stateList = savedInstanceState.getParcelableArrayList<Hero>(STATE_LIST)
+            val stateMode = savedInstanceState.getInt(STATE_MODE)
+            setActionBarTitle(title)
+            if (stateList != null) {
+                list.addAll(stateList)
+            }
+            setMode(stateMode)
+        }
     }
     fun getListHeroes(): ArrayList<Hero> {
         val dataName = resources.getStringArray(R.array.data_name)
@@ -32,6 +56,12 @@ class MainActivity : AppCompatActivity() {
             listHero.add(hero)
         }
         return listHero
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_TITLE, title)
+        outState.putParcelableArrayList(STATE_LIST, list)
+        outState.putInt(STATE_MODE, mode)
     }
 
     private fun showRecyclerList() {
@@ -50,18 +80,32 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                title = "Mode List"
                 showRecyclerList()
             }
             R.id.action_grid -> {
+                title = "Mode Grid"
                 showRecyclerGrid()
             }
             R.id.action_cardview -> {
+                title = "Mode CardView"
+                showRecyclerCardView()
             }
         }
+        mode = selectedMode
+        setActionBarTitle(title)
     }
     private fun showRecyclerGrid() {
         rv_heroes.layoutManager = GridLayoutManager(this, 2)
         val gridHeroAdapter = GridHeroAdapter(list)
         rv_heroes.adapter = gridHeroAdapter
+    }
+    private fun showRecyclerCardView() {
+        rv_heroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rv_heroes.adapter = cardViewHeroAdapter
+    }
+    private fun setActionBarTitle(title: String?) {
+        supportActionBar?.title = title
     }
 }
